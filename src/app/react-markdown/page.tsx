bfbc1a27294a862443ba4bad92c3e84f892c2429
HotariTobu <hotari24tools@gmail.com>
2024-07-16 01:16:03 +0900

    add: react-markdown highlighter

--- src/app/react-markdown/page.tsx
+++ src/app/react-markdown/page.tsx
@@ -1,8 +1,26 @@
 import { sample } from "@/sample.md";
 import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 
 export default function Page() {
   return (
-    <Markdown>{sample}</Markdown>
+    <Markdown
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props
+          const match = /language-(\w+)/.exec(className || '')
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              language={match[1]}
+            >{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          )
+        }
+      }}>{sample}</Markdown>
   )
 }
