53ae6868fdb11046157c820c8fe2ee39e7e4d226
HotariTobu <hotari24tools@gmail.com>
2024-07-16 01:29:48 +0900

    add: markdown-to-jsx highlighter

--- src/app/markdown-to-jsx/page.tsx
+++ src/app/markdown-to-jsx/page.tsx
@@ -1,8 +1,40 @@
+'use client'
+
 import { sample } from "@/sample.md";
 import Markdown from "markdown-to-jsx";
+import { useRef, useEffect } from "react";
 
 export default function Page() {
   return (
-    <Markdown>{sample}</Markdown>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/styles/nord.min.css"
+      />
+      <script
+        crossorigin
+        src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/highlight.min.js"
+      ></script>
+      <Markdown
+        options={{
+          overrides: {
+            code(props) {
+              const ref = useRef<HTMLElement | null>(null)
+
+              useEffect(() => {
+                if (ref.current && props.className?.includes('lang-') && window.hljs) {
+                  window.hljs.highlightElement(ref.current)
+
+                  // hljs won't reprocess the element unless this attribute is removed
+                  ref.current.removeAttribute('data-highlighted')
+                }
+              }, [props.className, props.children])
+
+              return <code {...props} ref={ref} />
+            },
+          },
+        }}
+      >{sample}</Markdown>
+    </>
   )
 }
